00a95607fc6b8def699039ee212c98582a456133
SEC-MASUM <masum.jess42@gmail.com>
2022-10-19 23:27:22 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FormArray';
+  items!: FormArray;
+
+  prForm = this.fb.group({
+    code: new FormControl('', [Validators.required]),
+    company: new FormControl('', [Validators.required]),
+    prItem: this.fb.array([]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.AddNewRow();
+  }
+
+  AddNewRow() {
+    this.items = this.prForm.get('prItem') as FormArray;
+    this.items.push(this.GenerateRow());
+  }
+
+  removeItem(index: number) {
+    this.items = this.prForm.get('prItem') as FormArray;
+    this.items.removeAt(index);
+  }
+
+  get prItem() {
+    return this.prForm.controls['prItem'] as FormArray;
+  }
+
+  GenerateRow() {
+    return this.fb.group({
+      name: new FormControl('', [Validators.required]),
+      unit: new FormControl('', Validators.required),
+      unitPrice: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.prForm.value);
+  }
 }
+
+// <table formArrayName="prItem">
+//           <tbody>
+//             <tr *ngFor="let item of prItem.controls; let i = index">
+//               <td>
+//                 <input placeholder="Name" formControlName="name" />
+//               </td>
+//               <td>
+//                 <input placeholder="Unit" formControlName="unit" />
+//               </td>
+//               <td>
+//                 <input placeholder="Unit Price" formControlName="unitPrice" />
+//               </td>
+//               <td>
+//                 <button
+//                   mat-raised-button
+//                   color="accent"
+//                   (click)="removeItem(i)"
+//                 >
+//                   (-) Remove Item
+//                 </button>
+//               </td>
+//             </tr>
+//           </tbody>
+//         </table>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // MatSelectModule,
+    // HttpClientModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
